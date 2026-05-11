3e1cf9aa5bc9f28eeb9ef6afa425fa7d4c207281
Smeltier <connect.goncalves@gmail.com>
2026-05-11 10:08:53 -0300

    att: updating functions order in guassian_elimination.cpp

--- algorithms/math/matrix/gaussian_elimination.cpp
+++ algorithms/math/matrix/gaussian_elimination.cpp
@@ -2,46 +2,6 @@
 #include <cmath>
 #include <algorithm>
 
-std::vector<std::vector<double>> gaussian_elimination(const std::vector<std::vector<double>> &, const std::vector<double> &);
-std::vector<std::vector<double>> create_augmented_matrix(const std::vector<std::vector<double>> &, const std::vector<double> &);
-int find_pivot(const std::vector<std::vector<double>> &, const int, const int);
-void swap_lines(std::vector<std::vector<double>> &, const int, const int);
-
-std::vector<std::vector<double>> gaussian_elimination(const std::vector<std::vector<double>> &A, const std::vector<double> &B) {
-  std::vector<std::vector<double>> result = create_augmented_matrix(A, B);
-
-  const int ROWS = (int) result.size();
-  const int COLS = (int) result[0].size();
-
-  for (int j = 0; j < std::min(ROWS, COLS - 1); ++j) {
-    int pivot_row = find_pivot(result, j, j);
-
-    if (std::abs(result[pivot_row][j]) < 1e-15) {
-      continue;
-    }
-
-    if (pivot_row != j) {
-      swap_lines(result, j, pivot_row);
-    }
-
-    for (int i = j + 1; i < ROWS; ++i) {
-      const double k = result[i][j] / result[j][j];
-
-      for (int l = j; l < COLS; ++l) {
-        if (l == j) {
-          result[i][l] = 0;
-        }
-        else {
-          result[i][l] -= k * result[j][l];
-        }
-      }
-    }
-
-  }
-
-  return result;
-}
-
 void swap_lines(std::vector<std::vector<double>> &mat, const int L1, const int L2) {
   std::swap(mat[L1], mat[L2]);
 }
@@ -79,3 +39,37 @@ std::vector<std::vector<double>> create_augmented_matrix(const std::vector<std::
   return augmented_matrix;
 }
 
+std::vector<std::vector<double>> gaussian_elimination(const std::vector<std::vector<double>> &A, const std::vector<double> &B) {
+  std::vector<std::vector<double>> result = create_augmented_matrix(A, B);
+
+  const int ROWS = (int) result.size();
+  const int COLS = (int) result[0].size();
+
+  for (int j = 0; j < std::min(ROWS, COLS - 1); ++j) {
+    int pivot_row = find_pivot(result, j, j);
+
+    if (std::abs(result[pivot_row][j]) < 1e-15) {
+      continue;
+    }
+
+    if (pivot_row != j) {
+      swap_lines(result, j, pivot_row);
+    }
+
+    for (int i = j + 1; i < ROWS; ++i) {
+      const double k = result[i][j] / result[j][j];
+
+      for (int l = j; l < COLS; ++l) {
+        if (l == j) {
+          result[i][l] = 0;
+        }
+        else {
+          result[i][l] -= k * result[j][l];
+        }
+      }
+    }
+
+  }
+
+  return result;
+}

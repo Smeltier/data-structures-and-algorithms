d5dca6903e4127f9581a5d0e4009c395f93ffe2d
Smeltier <connect.goncalves@gmail.com>
2026-05-25 17:22:59 -0300

    att: transfers the Hash Table implementation from .cpp to .hpp

--- data-structures/hash-table/hash_table.hpp
+++ data-structures/hash-table/hash_table.hpp
@@ -1,32 +1,133 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
+#include <functional>
 
+template <typename key_type, typename value_type>
 class HashTable {
 private:
-    std::vector<int> table;
+    struct Slot {
+        key_type key;
+        value_type value;
+        enum class State { EMPTY, OCCUPIED, TOMBSTONE } state = State::EMPTY;
+    };
+
+    std::vector<Slot> table;
     size_t current_size;
 
-    int hash(const int key) const;
+    int hash(const key_type key) const {
+        return std::hash<key_type>{}(key) % table.size();
+    }
 
 public:
-    HashTable();
+    HashTable() : table(0), current_size(0) {}
 
-    HashTable(const size_t size);
+    HashTable(const size_t size) : table(size), current_size(0) {}
 
-    void insert(const int key);
+    void insert(const key_type key, const value_type value) {
+        if (full()) {
+            return;
+        }
 
-    void remove(const int key);
+        int index = hash(key);
+        int tombstone_index = -1;
 
-    bool search(const int key) const;
+        for (size_t i = 0; i < table.size(); ++i) {
+            if (table[index].state == Slot::State::OCCUPIED and table[index].key == key) {
+                table[index].value = value;
+                return;
+            }
+            else if (table[index].state == Slot::State::TOMBSTONE) {
+                tombstone_index = (tombstone_index == -1) ? index : tombstone_index;
+            }
+            else {
+                int target = (tombstone_index != -1) ? tombstone_index : index;
+                table[target] = { key, value, Slot::State::OCCUPIED };
+                current_size++;
+                return;
+            }
 
-    void print() const;
+            index = (index + 1) % (int)table.size();
+        }
 
-    size_t max_size() const;
+        if (tombstone_index != -1) {
+            table[tombstone_index] = { key, value, Slot::State::OCCUPIED };
+            current_size++;
+        }
+    }
 
-    size_t size() const;
+    void remove(const key_type key) {
+        if (empty()) {
+            return;
+        }
 
-    bool empty() const;
+        int index = hash(key);
 
-    bool full() const;
+        for (size_t i = 0; i < table.size(); ++i) {
+            if (table[index].state == Slot::State::EMPTY) {
+                return;
+            }
+
+            if (table[index].state == Slot::State::OCCUPIED and table[index].key == key) {
+                table[index].state = Slot::State::TOMBSTONE;
+                current_size--;
+                return;
+            }
+
+            index = (index + 1) % (int)table.size();
+        }
+    }
+
+    bool search(const key_type key) const {
+        if (empty()) {
+            return false;
+        }
+
+        int index = hash(key);
+
+        for (size_t i = 0; i < table.size(); ++i) {
+            if (table[index].state == Slot::State::EMPTY) {
+                return false;
+            }
+
+            if (table[index].state == Slot::State::OCCUPIED and table[index].key == key) {
+                return true;
+            }
+
+            index = (index + 1) % (int)table.size();
+        }
+
+        return false;
+    }
+
+    void print() const {
+        for (Slot element : table) {
+            if (element.state == Slot::State::EMPTY) {
+                std::cout << "_ ";
+            }
+            else if (element.state == Slot::State::TOMBSTONE) {
+                std::cout << "* ";
+            }
+            else {
+                std::cout << "<" << element.key << " : " << element.value << ">";
+            }
+        }
+    }
+
+    size_t max_size() const {
+        return table.size();
+    }
+
+    size_t size() const {
+        return current_size;
+    }
+
+    bool empty() const {
+        return current_size == 0;
+    }
+
+    bool full() const {
+        return current_size == table.size();
+    }
 };
